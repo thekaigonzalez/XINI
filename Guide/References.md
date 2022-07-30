56235029e610f3b4a35ff684bb55f11fa8ba50c7
Kai D. Gonzalez <gkai70264@gmail.com>
2022-07-29 22:08:39 -0400

    Add referencing

---
+++ Guide/References.md
@@ -0,0 +1,11 @@
+# Referencing
+
+You can reference a variable from another class, simply by including the **ampersand** symbol (&) and typing the address of the value.
+
+Example:
+
+```ini
+hello.world = Hello, world!
+
+goodbye.world = &hello.world
+```
--- xini/analyzer.js
+++ xini/analyzer.js
@@ -2,6 +2,40 @@
 // Author: Kai Gonzalez
 // An object-oriented version of the INI file format.
 
+module.exports.traverseClass = (obj, str) => {
+    if (obj == null || typeof obj != "object") return null
+
+    let place = "";
+    let curnode;
+
+    for (let i = 0; i < str.length; ++i) {
+        if (str[i] == '.') {
+            if (curnode == null) {
+                curnode = obj[place]
+            } else {
+
+                if (curnode[place] != null && typeof curnode[place] != 'object')
+                    return curnode[place]
+                else {
+                    if (debug) {
+                        console.log("debug: deepslate: traversing node " + place)
+                    }
+                    curnode = curnode[place]
+                }
+            }
+            place = "";
+        }
+        else {
+            place = place + str[i];
+        }
+    }
+
+    if (place.length > 0 && curnode[place] != null) return curnode[place]
+
+    return curnode;
+
+}
+
 module.exports.parseXINI = (str) => {
     let cfg = {}
 
@@ -26,8 +60,22 @@ module.exports.parseXINI = (str) => {
             state = 1;
             name = buffer;
             buffer = ""
+        } else if (str[i] == '&' && state == 1) {
+            state = -1;
+            buffer = ""
         } else if (str[i] == '\n' && state == 1) {
             current_pos[name.trim()] = buffer.trim();
+            current_pos = cfg
+            name = "";
+            buffer = "";
+            state = 0
+        } else if (str[i] == '\n' && state == -1) {
+            var ref = this.traverseClass(cfg, buffer.trim())
+
+            if (ref == null)
+                console.warn("warning: reference not found.")
+
+            current_pos[name.trim()] = ref;
             current_pos = cfg
             name = "";
             buffer = "";
@@ -51,7 +99,20 @@ module.exports.parseXINI = (str) => {
         name = "";
         buffer = "";
         state = 0
+    } else if (buffer.length > 0 && state == -1) {
+        var ref = this.traverseClass(cfg, buffer.trim())
+
+        if (ref == null)
+            console.warn("warning: reference not found.")
+
+        current_pos[name.trim()] = ref;
+        current_pos = cfg
+        name = "";
+        buffer = "";
+        state = 0
     }
 
     return cfg
 }
+
+module.exports.version = "1.2"
